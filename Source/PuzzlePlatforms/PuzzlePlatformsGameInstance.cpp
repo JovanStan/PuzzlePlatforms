db94bb10b7216d1f2850f3db82f9ca5851f64a09
Jovan <jovanstankovic561@gmail.com>
2025-09-09 14:15:16 +0200

    Server Travel

---
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -0,0 +1,33 @@
+﻿
+
+#include "PuzzlePlatformsGameInstance.h"
+
+UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitializer& ObjectInitializer)
+{
+	
+}
+
+void UPuzzlePlatformsGameInstance::Init()
+{
+	Super::Init();
+}
+
+void UPuzzlePlatformsGameInstance::Host()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, "Hosting Game");
+
+	GetWorld()->ServerTravel("/Game/ThirdPerson/Lvl_ThirdPerson?listen");
+}
+
+void UPuzzlePlatformsGameInstance::Join(const FString& IP)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Joining Game at %s"), *IP));
+
+	APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
+
+	if (PlayerController)
+	{
+		PlayerController->ClientTravel(IP, TRAVEL_Absolute);
+	}
+}
+
---
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.h
@@ -0,0 +1,23 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "PuzzlePlatformsGameInstance.generated.h"
+
+
+UCLASS()
+class PUZZLEPLATFORMS_API UPuzzlePlatformsGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	UPuzzlePlatformsGameInstance(const FObjectInitializer& ObjectInitializer);
+	virtual void Init() override;
+
+	UFUNCTION(Exec)
+	void Host();
+
+	UFUNCTION(Exec)
+	void Join(const FString& IP);
+};
